64389d673e74146ef262cf2d71c2af0233f2533d
kgitti <kitti.suksong@gmail.com>
2014-08-17 15:21:34 +0700

    Add Abstract Test Suite

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 simple
 ======
 
-Do any test thing
+Create easy hello world project with Junit Test tool
--- src/helloworld/test/helloworld/HelloworldTestSuite.java
+++ src/helloworld/test/helloworld/HelloworldTestSuite.java
@@ -3,11 +3,13 @@ package helloworld;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import testCase.AbstractTestSuite;
+
 import helloworld.HelloTestCase;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { HelloTestCase.class })
-public class HelloworldTestSuite {
+public class HelloworldTestSuite extends AbstractTestSuite {
 	public static void debug(){
 		HelloTestCase.setDebug(true);
 	}
